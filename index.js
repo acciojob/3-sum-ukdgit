0039645b01a3ff2b83fd083eaeb3840c4109853f
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-09 21:13:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 // We will understand all of this later in the course.
 // DO NOT MODIFY THIS FILE
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser')
-
+const express = require("express");
+const path = require("path");
+const threeSum = require("./script");
 const app = express();
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/main.html"));
+});
 
-const majorElement = require('./script');
-
-app.use(express.static(__dirname))
-
-app.post('/majorityElementChecker',(req, res) => {
-  const arr = req.body.arr
-  const answer = majorElement(arr) 
-  res.send({major:answer})
-})
+app.post("/threesum", (req, res) => {
+  const { arr, target } = req.body;
+  const ans = threeSum(arr, target);
+  res.send({ message: ans });
+});
 
 module.exports = app;
